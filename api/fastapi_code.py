94d6d2655913cefda5183c22fe52078226b9a682
Leonardo <leonardob26@gmail.com>
2021-02-25 14:03:56 -0500

    Adding first api file

---
+++ api/fastapi_code.py
@@ -0,0 +1,15 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home1():
+    return {"Hello": "World"}
+    
+@app.get("/first/{id}")
+def first(id: int):
+    return {"Hello": id}    
+
+if __name__ == "__main__":
+    uvicorn.run("fastapi_code:app")
