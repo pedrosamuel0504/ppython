37021fbc8854bd4364f67853df6b30595bd36a04
Leonardo <leonardob26@gmail.com>
2021-03-23 00:42:36 -0400

    Nada que ver

--- api/fastapi_code.py
+++ api/fastapi_code.py
@@ -1,7 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseSettings
 
+class Settings(BaseSettings):
+    message: str
+
+settings = Settings()
 app = FastAPI()
+
+@app.get("/settings")
+def get_settings():
+    return { "message": settings.message }
+
 
 @app.get("/")
 def home1():
@@ -10,6 +20,8 @@ def home1():
 @app.get("/first/{id}")
 def first(id: int):
     return {"Hello": id}    
+
+    
 
 if __name__ == "__main__":
     uvicorn.run("fastapi_code:app",reload=True)
